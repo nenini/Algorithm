b1aa04fef89ba5fdfd9428ca24a0bcb106b9388d
nenini <kyr068600@gmail.com>
2025-03-16 01:59:48 +0900

    day-25/03/16/Sun

---
+++ backtracking/BOJ_15649.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int N, M;
+int arr[9];
+int visited[9];
+void dfs(int m)
+{
+    if(m==M){
+        for (int i = 0; i < M;i++){
+            if(i<M-1){
+                cout << arr[i] << " ";
+            }else{
+                cout << arr[i];
+            }
+        }
+        cout << "\n";
+        return ;
+    }else{
+        for (int i = 1; i <= N;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[m] = i;
+                dfs(m + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin >> N >> M;
+    dfs(0);
+}
